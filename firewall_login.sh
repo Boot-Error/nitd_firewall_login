c4f9b9cc983696252004ccf798aa64b79d53ca79
Boot-Error <booterror99@gmail.com>
2019-11-04 15:21:18 +0530

    Added CURL_FLAGS and HEADERS

--- firewall_login.sh
+++ firewall_login.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CURL_FLAGS="-s"
 FIREWALL_IP="http://172.16.1.1:1000/"
 HEADERS="-H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1'"
 
@@ -9,27 +10,28 @@ flogin() {
 	PASSWD="$2"
 
 	# first ask for login page
-	LOGIN_PAGE=$(curl $FIREWALL_IP/login?)
+	LOGIN_PAGE=$(curl $CURL_FLAGS $HEADERS $FIREWALL_IP/login?)
 
 	MAGIC_VALUE=$(echo $LOGIN_PAGE | grep -oP '(?<=<input type="hidden" name="magic" value=")[0-9a-z]+(?=">)')
 
 	echo "Logging with USER = $USERNAME, PASSWD = $PASSWD, MAGIC = $MAGIC_VALUE"
 
 	# login with $USERNAME $MAGIC $PASSWD
-	RESULT=$(curl $FIREWALL_IP \
+	RESULT=$(curl $CURL_FLAGS $FIREWALL_IP \
 		-H "referer: $FIREWALL_IP/login?" \
+		$HEADERS \
 		--data "4Tredir=http%3A%2F%2F172.16.1.1%3A1000%2Flogin%3F&magic=$MAGIC_VALUE&username=$USERNAME&password=$PASSWD")
 
 }
 
 flogout() {
 	
-	curl "$FIREWALL_IP/logout?"
+	curl $CURL_FLAGS "$FIREWALL_IP/logout?"
 }
 
 fkeepalive() {
 
-	curl "$FIREWALL_IP/keepalive?0000000000000000000"
+	curl $CURL_FLAGS "$FIREWALL_IP/keepalive?0000000000000000000"
 }
 
 case "$1" in
